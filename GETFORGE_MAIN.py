3d6ff8b01685a53dc1843453dbac1d8f67730691
SuHouyu <553070703@qq.com>
2019-05-28 03:17:44 +0800

    Add files via upload

--- GETFORGE_MAIN.py
+++ GETFORGE_MAIN.py
@@ -8,6 +8,7 @@ class WIN( Ui_MainWindow,QtWidgets.QMainWindow ):
     text = ''
     ForgeVersion = ''
     FILETYPE = ''
+    Content = ['']# 用来储存网页信息等
     def __init__( self ):
         super(WIN,self).__init__()
         self.setupUi( self )
@@ -15,12 +16,14 @@ class WIN( Ui_MainWindow,QtWidgets.QMainWindow ):
         self.ButtonSolve()
         #self.textBrowser.append("1." "<a href='#'>https://files.minecraftforge.net/maven/net/minecraftforge/f""orge/1.12.2-14.23.5.2838/forge-1.12.2-14.23.5.2838-installer-win.exe</a><br>" )
 
-    def ButtonSolve(self):
+    def ButtonSolve(self): #检查事件
         self.pushButton.clicked.connect(self.Get)
         self.pushButton_2.clicked.connect(self.Dowload_Laster)
 
     
-    def Dowload_Laster(self):
+    def Dowload_Laster(self):   #点击下载时的操作
+        self.Content[0] = ''#初始化Content
+
         try:
             self.NewestAddr = self.ForgeAddressList[-1]
         except:
@@ -28,16 +31,23 @@ class WIN( Ui_MainWindow,QtWidgets.QMainWindow ):
 
         res = urllib.request.urlopen( self.NewestAddr )
         # print ( self.NewestAddr )
-        theForgeData = res.read()
-        print (theForgeData)
+        file = open("Forge" + self.ForgeVersion + "." + self.FILETYPE, "wb")
 
-        file = open( "Forge" + self.ForgeVersion + "." + self.FILETYPE ,"ab" )
-
-        file.write( theForgeData )
+        while True:
+            theForgeData = res.readline()
+            file.write(theForgeData)
+            # self.Content[0] += str(theForgeData)
+            if not theForgeData:
+                break
 
         file.close()
 
-    def Get(self):
+        QtWidgets.QMessageBox.question(self,'提示','文件下载完毕'
+                                       ,QtWidgets.QMessageBox.Yes |
+                                       QtWidgets.QMessageBox.No,
+                                       QtWidgets.QMessageBox.No)
+
+    def Get(self):  #取得combobox的内容并判断
         self.text = self.comboBox.currentIndex()
 
 
@@ -57,7 +67,7 @@ class WIN( Ui_MainWindow,QtWidgets.QMainWindow ):
             self.SELECT('jar')
 
 
-    def SELECT(self,FILETYPE):
+    def SELECT(self,FILETYPE):# 指定内容爬取
         self.FILETYPE = FILETYPE
         self.textBrowser.clear()
 
@@ -68,21 +78,27 @@ class WIN( Ui_MainWindow,QtWidgets.QMainWindow ):
         
         try:
             respond = urllib.request.urlopen( self.URL )
-            websiteInfo = respond.read().decode('utf-8')
+
+            while True:#逐行取得网页内容
+                websiteInfo = respond.readline().decode('utf-8')
+                self.Content[0] += websiteInfo
+                if not websiteInfo:#检查到文件尾时退出读取
+                    break
+
             SelRuler = r"https*://file.*" + FILETYPE  # 筛选规则
             ob = re.compile(SelRuler)
-            self.ForgeAddressList = ob.findall(websiteInfo)
+            self.ForgeAddressList = ob.findall(self.Content[0])
 
 
             print(self.ForgeAddressList[0])
-            ForgeAddressList = sorted(self.ForgeAddressList)
+            ForgeAddressList = sorted(self.ForgeAddressList,reverse=True)
         
             count = 1
             for i in self.ForgeAddressList:
                 self.textBrowser.append( str( count ) + '.' + '<a href =' + i + ">" + i + "</a>" + '<br>' )
                 count += 1
         except:
-            QtWidgets.QMessageBox.question(self,'警告!','版本输入有误！',
+            QtWidgets.QMessageBox.question(self,'警告!','版本输入有误或文件类型不存在，请选择其他类型',
                                    QtWidgets.QMessageBox.Yes |
                                    QtWidgets.QMessageBox.No ,
                                    QtWidgets.QMessageBox.Yes)
